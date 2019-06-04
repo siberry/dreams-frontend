bea39d03e57d4734ced3feebd35bc1d97fbb1153
siberry <siberry226@gmail.com>
2019-06-04 18:19:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  componentDidMount() {
+    fetch("http://localhost:3000/dream_tags")
+    .then(res => res.json())
+    .then(console.log)
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
